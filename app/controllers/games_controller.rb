b5ac07e93f8aa50aeac4df2067345b74ab893351
Melissa Holmes <meltar@gmail.com>
2014-12-13 15:55:11 -0500

    Add controller logic and delete old file

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,24 @@
 class GamesController < ApplicationController
+
   def index
+		@games = Game.all
+		respond_to do |format|
+			format.html
+			format.json { render json: @games }
+		end
   end
 
   def new
+		@game = Game.new
   end
 
-  def index
+  def create
+		@game = Game.new(game_params)
+		if @game.save
+			redirect_to 'games#index'
+		else
+			# show error?
+			render 'new'
+		end
   end
 end
